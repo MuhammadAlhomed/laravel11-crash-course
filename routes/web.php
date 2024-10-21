6b9f4af2439812d994e720f3820136c1046b7aaa
Muhammad Alhomed <m.alhumed@qu.edu.sa>
2024-10-21 14:39:04 +0300

    minor improvements

--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('note', NoteController::class)->names('note');
+Route::resource('/note', NoteController::class)->names('note');
+Route::get('/register', function () {
+    return view('auth.register');
+});
+Route::post('/register', function () {
+    $request = request()->validate([
+        'email' => ['required'],
+    ]);
+    return redirect()->to('/note')->with('message', 'Login successful');
+});
