ab8be58187597cd81805c28c71dc0c100813607d
MuhammadAlhomed <MOoOHMMD24@hotmail.com>
2025-10-12 20:00:21 +0300

    improved navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,14 +1,26 @@
 <nav class="navbar navbar-light bg-white border-bottom shadow-sm justify-content-between px-2">
-    <a class="navbar-brand" href='{{ route('note.index') }}'><h3 class="m-0">{{config('app.name')}}</h3></a>
+    <a class="navbar-brand p-0" href='{{ route('note.index') }}'><h3 class="m-0">{{config('app.name')}}</h3></a>
 
     {{-- AUTH --}}
     @auth
-        <div class="d-flex gap-1">
-            <h3 class="m-0">Welcome {{auth()->user()->username}}!</h3>
+        <div class="d-flex flex-row-reverse align-items-center gap-3">
             <form action="{{route('auth.logout')}}" method="post" class="form-inline">
                 @csrf
                 <button class="btn btn-primary" type="submit">Logout</button>
             </form>
+            <div class="dropdown open">
+                <h3 class="m-0 me-3">
+                    <span class="text-muted fs-5">Welcome </span>
+                    <span style="cursor: pointer" data-bs-toggle="dropdown" aria-expanded="false">{{auth()->user()->username}}!</span>
+                    <div class="dropdown-menu dropdown-menu-end bg-white" aria-labelledby="userDropdown" >
+                        <a class="dropdown-item" href="{{ route('profile') }}">Edit profile</a>
+                        {{-- <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">After divider action</a> --}}
+                    </div>
+                </h3>
+
+            </div>
+
         </div>
     @endauth
 
