2043ba6a3ef6840bc0506647118366a58516cfee
MuhammadAlhomed <MOoOHMMD24@hotmail.com>
2025-10-15 21:09:52 +0300

    updated form input to support textarea

--- resources/views/components/form-input.blade.php
+++ resources/views/components/form-input.blade.php
@@ -3,15 +3,26 @@
     'value' => null,
     'type' => 'text',
     ])
+@if($type === 'textarea')
+<textarea
+    class="form-control @error($name) is-invalid @enderror"
+    id="{{$name}}"
+    type={{$type}}
+    name={{$name}}
+    {{$attributes}}
+    >{{$value}}</textarea>
 
-<input 
-    class="form-control @error($name) is-invalid @enderror" 
-    id="{{$name}}" 
-    type={{$type}} 
-    name={{$name}} 
-    @if($value) value="{{$value}}" @endif 
+@else
+
+<input
+    class="form-control @error($name) is-invalid @enderror"
+    id="{{$name}}"
+    type={{$type}}
+    name={{$name}}
+    @if($value) value="{{$value}}" @endif
     {{$attributes}}
     />
+@endif
 @error($name)
 <div id="{{$name}}Error" class="invalid-feedback">{{$errors->first($name) ?? 'Invalid Input'}}</div>
 @enderror
