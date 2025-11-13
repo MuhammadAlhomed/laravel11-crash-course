66c95c8ca6d5fe1fd99c8c6266a57c870f046b27
MuhammadAlhomed <MOoOHMMD24@hotmail.com>
2025-11-13 20:49:40 +0300

    added very crude websocket

--- resources/views/note/index.blade.php
+++ resources/views/note/index.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
     <a href={{route('note.create')}} class="btn btn-primary d-inline-flex align-items-center"><i data-feather="plus" class="align-middle me-2"></i>New Note</a>
 
-    <div class="my-3">
+    <div id="notesFeed" class="my-3">
     @foreach ($notes as $note)
         <div class="my-3">
             <x-note :note="$note"/>
